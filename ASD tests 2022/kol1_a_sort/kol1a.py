3dfb648f7b11ab6509fe1b301254ec9b8f8851a6
xNitix <dziwakdawid@gmail.com>
2024-03-18 01:43:31 +0100

    uporzadkowanie

--- ASD tests 2022/kol1_a_sort/kol1a.py
+++
@@ -1,9 +0,0 @@
-from kol1atesty import runtests
-
-def g(T):
-    # tu prosze wpisac wlasna implementacje
-    return -1
-
-
-# Zamien all_tests=False na all_tests=True zeby uruchomic wszystkie testy
-runtests( g, all_tests=False )
